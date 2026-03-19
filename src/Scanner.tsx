66d5760d4057a47fcfd03943b9c80b60c64943dc
swapno963 <swapno963@gmail.com>
2026-03-19 12:53:58 +0600

    Extracting scanned value, barcode type, time-stamp and devide-browser info

--- src/Scanner.tsx
+++ src/Scanner.tsx
@@ -7,7 +7,12 @@ const Scanner: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const controlsRef = useRef<IScannerControls | null>(null);
   const readerRef = useRef<BrowserMultiFormatReader | null>(null);
-
+  type ScanResult = {
+    value: string;
+    format: string;
+    timestamp: string;
+    device: string;
+  };
   const [result, setResult] = useState<string>("");
   const [error, setError] = useState<string>("");
   const [isScanning, setIsScanning] = useState<boolean>(false);
@@ -24,7 +29,9 @@ const Scanner: React.FC = () => {
 
     readerRef.current = new BrowserMultiFormatReader(hints);
   };
-
+  const getDeviceInfo = (): string => {
+    return navigator.userAgent;
+  };
   const startScanner = async () => {
     if (!videoRef.current || isScanning) return;
 
@@ -43,6 +50,13 @@ const Scanner: React.FC = () => {
         (res) => {
           if (res) {
             const text = res.getText();
+             const scanData: ScanResult = {
+                        value: res.getText(),
+                        format: res.getBarcodeFormat().toString(),
+                        timestamp: new Date().toISOString(),
+                        device: getDeviceInfo(),
+                      };
+            console.log("The scanData is : ", scanData)
             setResult(text);
 
             // Stop after successful scan
