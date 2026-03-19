46ab82cc4031afbcab4fc5621b72bf726a26d98c
swapno963 <swapno963@gmail.com>
2026-03-19 21:39:10 +0600

    Login and registration are now pulic routes and scanner and dashboard is protected

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import Scanner from "./Scanner";
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Scanner from './pages/Scanner';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Registration from './pages/Registration';
+import ProtectedRoute from './routes/ProtectedRoute';
+import PublicRoute from './routes/PublicRoute';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Scanner />
-    </div>
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route
+          path="/login"
+          element={
+            <PublicRoute restricted={true}>
+              <Login />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/registration"
+          element={
+            <PublicRoute restricted={true}>
+              <Registration />
+            </PublicRoute>
+          }
+        />
+
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Scanner />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
