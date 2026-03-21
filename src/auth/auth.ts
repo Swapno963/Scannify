a6ec82a2e0fbcd014795b54e54f8da09c720e2e6
swapno963 <swapno963@gmail.com>
2026-03-21 18:34:09 +0600

    get toekn and onLogout function created

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,3 +1,12 @@
 export const isAuthenticated = (): boolean => {
   return !!localStorage.getItem('token');
 };
+
+  export const getToken = (): string | null => {
+  return localStorage.getItem("token");
+};
+export const onLogout = (): void => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("email");
+};
